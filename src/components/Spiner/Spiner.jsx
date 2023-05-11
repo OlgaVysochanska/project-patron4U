1be386680c71279f4b8da5cf741e7781d7000185
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2023-05-11 21:35:32 +0300

    add spiner

---
+++ src/components/Spiner/Spiner.jsx
@@ -0,0 +1,14 @@
+import styles from'./spiner.module.scss';
+import paw from './spiner_paw.svg';
+
+
+
+const Spiner = () => {
+  return (
+    <div className={styles.paw}>
+      <img src={paw} alt="Your paw" />
+    </div>
+  );
+};
+
+export default Spiner;
