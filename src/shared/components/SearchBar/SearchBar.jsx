d1a9a1e9b9544243f0a5465aa4bf273cec43c1c9
28Rostyk <107403659+28Rostyk@users.noreply.github.com>
2023-05-12 13:42:31 +0300

    fixed and add style

--- src/shared/components/SearchBar/SearchBar.jsx
+++ src/shared/components/SearchBar/SearchBar.jsx
@@ -1,34 +1,27 @@
 import Button from 'components/Button/Button';
 import SearchIcon from '../../../icons/SearchIcon';
-// import CrossIcon from 'icons/CrossIcon';
+import CrossIcon from 'icons/CrossIcon';
 import useForm from 'shared/hooks/useForm';
 import initialState from './initialState';
 
 import style from './SearchBar.module.scss';
 
 const SearchBar = ({ onSubmit }) => {
-  //   const [searchQuery, setSearchQuery] = useState('');
-
-  //   const handleQuerySearch = e => {
-  //     setSearchQuery(e.target.value.toLowerCase());
-  //   };
-
-  //   const handleSubmit = e => {
-  //     e.preventDefault();
-  //     // if (searchQuery.trim() === '') {
-  //     //   toast.info('Enter the film title');
-  //     // }
-
-  //     // onSearch(searchQuery);
-  //     setSearchQuery('');
-  //   };
-
-  const { state, handleChange, handleSubmit } = useForm({
+  const { state, setState, handleChange, handleSubmit } = useForm({
     initialState,
     onSubmit,
   });
   console.log('Render form');
   const { search } = state;
+
+  const buttonClass = search ? style.active : style.disabled;
+
+  const remove = () => {
+    setState({
+      search: '',
+    });
+  };
+
   return (
     <form className={style.searchForm} onSubmit={handleSubmit}>
       <div className={style.container}>
@@ -46,13 +39,18 @@ const SearchBar = ({ onSubmit }) => {
 
         <Button
           type="submit"
-          className={style.searchBtn}
+          className={buttonClass}
           SVGComponent={SearchIcon}
         />
 
-        {/* {search && (
-          <Button className={style.crossBtn} SVGComponent={CrossIcon} />
-        )} */}
+        {search && (
+          <Button
+            type="button"
+            className={style.crossBtn}
+            SVGComponent={CrossIcon}
+            onClick={remove}
+          />
+        )}
       </div>
     </form>
   );
