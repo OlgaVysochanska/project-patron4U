78879bc3bd6c0eb08fc6646b80216810dad17d68
OlgaVysochanska <olavysochanska@gmail.com>
2023-05-14 00:32:52 +0300

    add reexport

---
+++ src/shared/components/Button/Button.jsx
@@ -0,0 +1,20 @@
+function Button(props) {
+  const {
+    onClick,
+    label,
+    className,
+    SVGComponent,
+    showLabelFirst = true,
+  } = props;
+
+  return (
+    <button onClick={onClick} className={className}>
+      {showLabelFirst && label}
+      {SVGComponent && <SVGComponent />}
+      {!showLabelFirst && label}
+      {props.children}
+    </button>
+  );
+}
+
+export default Button;
