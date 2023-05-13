12728fa2884c7302a03238cdfa9137c176485373
OlgaVysochanska <olavysochanska@gmail.com>
2023-05-13 13:56:02 +0300

    reexport in pages

---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,7 @@
+const MainPage = () => {
+  return (
+    <div>If components are ready, add them to MainPage. /h1, img, bg/</div>
+  );
+};
+
+export default MainPage;
---
+++ src/pages/OurFriendsPage/OurFriendsPage.jsx
@@ -0,0 +1,7 @@
+const OurFriendsPage = () => {
+  return (
+    <div>If components are ready, add them to OurFriendsPage. /Friends/</div>
+  );
+};
+
+export default OurFriendsPage;
