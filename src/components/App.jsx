d7cbec8bef88eb8a84613b01ae81aa6a70e034d7
OlgaVysochanska <olavysochanska@gmail.com>
2023-05-13 13:16:11 +0300

    edit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,10 @@ import { Route, Routes } from 'react-router-dom';
 
 import { SharedLayout } from './SharedLayout/SharedLayout';
 
-import ModalApprovAction from 'shared/components/ModalApprovAction/ModalApprovAction';
-
-import ModalCongrats from './ModalCongrats/ModalCongrats';
-
 export const App = () => {
   return (
-    <ModalCongrats />
-    // <Routes>
-    //   <Route path="/" element={<SharedLayout />}></Route>
-    // </Routes>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}></Route>
+    </Routes>
   );
 };
