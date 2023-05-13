015e683c7cc9bc5ae374bfb540e9988a1bfb9026
OlgaVysochanska <olavysochanska@gmail.com>
2023-05-13 20:20:12 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,11 +5,11 @@ import { Provider } from 'react-redux';
 
 import { SharedLayout } from './SharedLayout/SharedLayout';
 
-          import RegisterPage from '../pages/RegisterPage/RegisterPage';
+import RegisterPage from '../pages/RegisterPage/RegisterPage';
 import LoginPage from 'pages/LoginPage/LoginPage';
 
 import { store, persistor } from '../redux/store';
-          
+
 const MainPage = lazy(() => import('pages/MainPage'));
 const NoticesPage = lazy(() => import('pages/NoticesPage'));
 const NewsPage = lazy(() => import('pages/NewsPage'));
@@ -20,23 +20,22 @@ const NotFound = lazy(() => import('pages/NotFound'));
 
 export const App = () => {
   return (
-    <Privider store={store}>
-    <Suspense fallback={<p>...Loading</p>}>
-      <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route index element={<MainPage />} />
+    <Provider store={store}>
+      <Suspense fallback={<p>...Loading</p>}>
+        <Routes>
+          <Route path="/" element={<SharedLayout />}>
+            <Route index element={<MainPage />} />
             <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
-          <Route path="/notices" element={<NoticesPage />} />
-          <Route path="/friends" element={<OurFriendsPage />} />
-          <Route path="/news" element={<NewsPage />} />
-          <Route path="/user" element={<UserPage />} />
-          <Route path="/add-pet" element={<AddPetPage />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </Suspense>
-</Provider>
-
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/notices" element={<NoticesPage />} />
+            <Route path="/friends" element={<OurFriendsPage />} />
+            <Route path="/news" element={<NewsPage />} />
+            <Route path="/user" element={<UserPage />} />
+            <Route path="/add-pet" element={<AddPetPage />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </Provider>
   );
 };
