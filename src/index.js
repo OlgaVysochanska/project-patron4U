dcf9215247a9cc62d859017372131ce9c31a241f
Ievgeniia Melnyk <agnikava@gmail.com>
2023-05-13 22:09:52 +0300

    add code

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import './index.css';
-import MainPage from 'pages/MainPage/MainPage';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
-      <MainPage />
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import scss from './MainPage.module.scss';
 
-// import bgImgMob from '../../images/bg_320x601(543).png';
-// import bgImgTab from '../../images/bg_768x1193(1125).png';
-// import bgImgDes from '../../images/bg_1280x770(708).png';
+import bgImgMob from '../../images/bg_320x601(543).png';
+import bgImgTab from '../../images/bg_768x1193(1125).png';
+import bgImgDes from '../../images/bg_1280x770(708).png';
 import petsMob1x from '../../images/pets_mob_1x.png';
-// import petsMob2x from '../../images/pets_mob_2x.png';
+import petsMob2x from '../../images/pets_mob_2x.png';
 import petsTab1x from '../../images/pets_tab_1x.png';
-// import petsTab2x from '../../images/pets_tab_2x.png';
+import petsTab2x from '../../images/pets_tab_2x.png';
 import petsDes1x from '../../images/pets_des_1x.png';
-// import petsDes2x from '../../images/pets_des_2x.png';
+import petsDes2x from '../../images/pets_des_2x.png';
 
 const MainPage = () => {
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
@@ -24,34 +24,48 @@ const MainPage = () => {
     };
   }, []);
 
-  //   const getImageBG = () => {
-  //     console.log(window.devicePixelRatio);
-  //     if (screenWidth < 768) {
-  //       return bgImgMob;
-  //     } else if (screenWidth >= 768 && screenWidth < 1280) {
-  //       return bgImgTab;
-  //     } else {
-  //       return bgImgDes;
-  //     }
-  //   };
-  const getImage = () => {
+  const getImageBG = () => {
     if (screenWidth < 768) {
-      return petsMob1x;
-    } else if (screenWidth >= 768 && screenWidth < 1200) {
-      return petsTab1x;
+      return bgImgMob;
+    } else if (screenWidth >= 768 && screenWidth < 1280) {
+      return bgImgTab;
     } else {
+      return bgImgDes;
+    }
+  };
+
+  const getImage = () => {
+    if (screenWidth < 768 && window.devicePixelRatio === 1) {
+      return petsMob1x;
+    } else if (screenWidth < 768 && window.devicePixelRatio === 2) {
+      return petsMob2x;
+    } else if (
+      screenWidth >= 768 &&
+      screenWidth < 1200 &&
+      window.devicePixelRatio === 1
+    ) {
+      return petsTab1x;
+    } else if (
+      screenWidth >= 768 &&
+      screenWidth < 1200 &&
+      window.devicePixelRatio === 2
+    ) {
+      return petsTab2x;
+    } else if (window.devicePixelRatio === 1) {
       return petsDes1x;
+    } else if (window.devicePixelRatio === 2) {
+      return petsDes2x;
     }
   };
 
   return (
     <>
-      {/* <img
+      <img
         className={scss.bg}
         src={getImageBG()}
         alt="back ground"
         loading="lazy"
-      /> */}
+      />
       <div className={scss.screen}>
         <img className={scss.pets} src={getImage()} alt="pets" loading="lazy" />
         <h1 className={scss.title}>Take good care of your small pets</h1>
