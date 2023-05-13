6d065ef9a21e5f381f87894772817dc4977bf48b
hlushak <75501308+MarynaHl@users.noreply.github.com>
2023-05-14 00:11:03 +0100

    components are changed to fit the overall structure, ourFriendsPage comleted

--- src/pages/OurFriendsPage/OurFriendsPage.jsx
+++ src/pages/OurFriendsPage/OurFriendsPage.jsx
@@ -1,7 +1,47 @@
+import s from './OurFriendsPage.module.scss';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import OurFriendsList from '../../components/Friends/OurFriendsList';
+import Spiner from '../../components/Spiner/Spiner';
+
+const translations = {
+en: {
+friendsPage: 'Friends Page'
+},
+uk: {
+friendsPage: 'Сторінка друзів'
+}
+};
+
+const Container = ({ children }) => {
+return <div className={s.container}>{children}</div>;
+};
+
 const OurFriendsPage = () => {
-  return (
-    <div>If components are ready, add them to OurFriendsPage. /Friends/</div>
-  );
+const [friends, setFriends] = useState([]);
+const [isLoading, setIsLoading] = useState(false);
+const [lang] = useState('en');
+
+useEffect(() => {
+setIsLoading(true);
+axios.get('/api/friends')
+.then(response => {
+setFriends(response.data);
+})
+.catch(error => console.log(error))
+.finally(() => setIsLoading(false));
+}, []);
+
+const t = (key) => {
+return translations[lang][key];
+};
+
+return (
+<Container>
+<h2 className={s.mainTitle}>{t('friendsPage')}</h2>
+{isLoading ? <Spiner /> : <OurFriendsList friends={friends} />}
+</Container>
+);
 };
 
 export default OurFriendsPage;
