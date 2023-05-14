a558c69851db1f2669edf8952b58252cd3e3cef7
DianaAkhmedova <106441711+DianaAkhmedova@users.noreply.github.com>
2023-05-14 12:10:49 +0300

    Update SharedLayout.jsx

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -2,13 +2,21 @@ import { Outlet } from 'react-router-dom';
 
 import Header from 'components/Header/Header';
 
+import NoticesCategoriesList from 'components/Notices/NoticesCategoriesList/NoticesCategoriesList';
+import PetsData from 'components/User/PetsData/PetsData';
+
+// import data from '../Notices/notices.json';
+import data from '../User/PetsData/pets.json';
+
 const SharedLayout = () => {
   return (
     <div>
       <Header />
 
       <main>
-        <Outlet />
+        {/* <NoticesCategoriesList data={data} /> */}
+        <PetsData data={data} />
+        {/* <Outlet /> */}
       </main>
     </div>
   );
