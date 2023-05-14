5ffb4f7acea3a5fb7ad16ef1379fca234c5f90e4
DianaAkhmedova <106441711+DianaAkhmedova@users.noreply.github.com>
2023-05-14 12:12:13 +0300

    Update PetsData.jsx

---
+++ src/components/User/PetsData/PetsData.jsx
@@ -0,0 +1,27 @@
+import Button from 'shared/components/Button/Button';
+import PetsList from './PetsList/PetsList';
+import PlusIcon from 'icons/PlusIcon';
+
+import styles from './PetsData.module.scss';
+
+const { petsDataContainer, petsHeader, petsTitle, addBtn, addBtnIcon } = styles;
+
+const PetsData = ({ data }) => {
+  return (
+    <div className={petsDataContainer}>
+      <div className={petsHeader}>
+        <h3 className={petsTitle}>My pets:</h3>
+        <Button
+          label="Add Pet"
+          className={addBtn}
+          SVGComponent={() => (
+            <PlusIcon className={addBtnIcon} color="#FEF9F9" />
+          )}
+        />
+      </div>
+      <PetsList data={data} />
+    </div>
+  );
+};
+
+export default PetsData;
