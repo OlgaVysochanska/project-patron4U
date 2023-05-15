2d73d2c6ad545ea0cb1d85a73a6dab317fd19d22
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2023-05-15 23:37:46 +0300

    add page data

---
+++ src/components/User/UserData/UserData.jsx
@@ -0,0 +1,90 @@
+import styles from './UserData.module.scss';
+import UserDataItem from './UserDataItem/UserDataItem';
+import useForm from 'shared/hooks/useForm';
+import { useState } from 'react';
+// import { ToggleButtonPhoto } from 'shared/components/ToggleButtonPhoto/ToggleButtonPhoto';
+import defaultAvatar from './default_avatar.svg';
+import Button from '../../../shared/components/Button/Button';
+import LogoutIcon from 'icons/LogoutIcon';
+import Logout from '../Logout/Logout';
+// import {nanoid} from 'nanoid'
+
+const picSize = '182px';
+
+let avatar = false;
+
+const user = {
+  name: 'Anna',
+  email: 'anna00@gmail.com',
+  birthday: '00.00.0000',
+  phone: '+38000000000',
+  city: 'Kiev',
+};
+
+const UserData = ({ onClick }) => {
+  const [isActive, setActive] = useState(false);
+  const [activeItem, setActiveItem] = useState('');
+  const [name, setName] = useState(user.name);
+  const [email, setEmail] = useState(user.email);
+  const [birthday, setBirthday] = useState(user.birthday);
+  const [phone, setPhone] = useState(user.phone);
+  const [city, setCity] = useState(user.city);
+
+  const userMap = { name, email, birthday, phone, city };
+
+  if (!avatar) {
+    avatar = defaultAvatar;
+  }
+
+  const clickToglle = id => {
+    onClick(!isActive);
+
+    console.log(activeItem);
+  };
+  const clickActive = id => {
+    !isActive ? setActiveItem(id) : setActiveItem(false);
+  };
+
+  console.log(userMap);
+  const elements = Object.entries(userMap).map(([key, value]) => {
+    console.log(key);
+    return (
+      <div>
+        <form>
+          <UserDataItem
+            label={key.charAt(0).toUpperCase() + key.slice(1)}
+            name={key}
+            value={value}
+            clickToglle={clickToglle}
+            isActive={isActive}
+            clickActive={clickActive}
+            activeItem={activeItem}
+            // id={nanoid()}
+          />
+        </form>
+      
+      </div>
+    );
+  });
+  console.log(elements);
+
+  return (
+    <div className={styles.container}>
+      <img src={avatar} alt="Your look" width={picSize} height={picSize}></img>
+      {/* <ToggleButtonPhoto>Edit photo</ToggleButtonPhoto> */}
+      <form>
+        {elements}
+        {/* <UserDataItem
+          label={name.charAt(0).toUpperCase() + name.slice(1)}
+          name={userMap[0]}
+          value={name}
+          clickToglle={clickToglle}
+          isActive={isActive}
+        /> */}
+      </form>
+      <Button label="Log Out" LogoutIcon />
+    </div>
+  );
+};
+
+export default UserData;
--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -1,6 +1,17 @@
+import Logout from 'components/User/Logout/Logout';
+// import PetsData from 'components/User/PetsData/PetsData';
+import UserData from 'components/User/UserData/UserData';
+
 const UserPage = () => {
   return (
     <div>
+      <div className="container">
+        <h2>My information:</h2>
+        <UserData />
+        <Logout />
+      </div>
+      <h2>My pets:</h2>
+      {/* <PetsData /> */}
       If components are ready, add them to UserPage. /UserData, UserDataItem,
       Logout, PetsData, PetsList, PetsItem, ModalCongrats/
     </div>
