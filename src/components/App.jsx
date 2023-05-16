2fb45e0dc0162df4334f1a7d440bbce4994e371b
GallaDindorogo <107935917+GallaDindorogo@users.noreply.github.com>
2023-05-16 17:28:14 +0200

    add DropdownSelectors

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,40 +1,44 @@
-import { Route, Routes } from 'react-router-dom';
-import { lazy, Suspense } from 'react';
+// import { Route, Routes } from 'react-router-dom';
+// import { lazy, Suspense } from 'react';
 
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 
-import SharedLayout from './SharedLayout/SharedLayout';
+// import SharedLayout from './SharedLayout/SharedLayout';
 
-import { store } from '../redux/store';
+// import { store } from '../redux/store';
 
-const MainPage = lazy(() => import('pages/MainPage'));
-const RegisterPage = lazy(() => import('pages/RegisterPage'));
-const LoginPage = lazy(() => import('pages/LoginPage'));
-const NoticesPage = lazy(() => import('pages/NoticesPage'));
-const NewsPage = lazy(() => import('pages/NewsPage'));
-const OurFriendsPage = lazy(() => import('pages/OurFriendsPage'));
-const UserPage = lazy(() => import('pages/UserPage'));
-const AddPetPage = lazy(() => import('pages/AddPetPage'));
-const NotFound = lazy(() => import('pages/NotFound'));
+// const MainPage = lazy(() => import('pages/MainPage'));
+// const RegisterPage = lazy(() => import('pages/RegisterPage'));
+// const LoginPage = lazy(() => import('pages/LoginPage'));
+// const NoticesPage = lazy(() => import('pages/NoticesPage'));
+// const NewsPage = lazy(() => import('pages/NewsPage'));
+// const OurFriendsPage = lazy(() => import('pages/OurFriendsPage'));
+// const UserPage = lazy(() => import('pages/UserPage'));
+// const AddPetPage = lazy(() => import('pages/AddPetPage'));
+// const NotFound = lazy(() => import('pages/NotFound'));
+
+import NoticesFilters from '../components/Notices/NoticesFilters';
 
 export const App = () => {
-  return (
-    <Provider store={store}>
-      <Suspense fallback={<p>...Loading</p>}>
-        <Routes>
-          <Route path="/" element={<SharedLayout />}>
-            <Route path="/main" element={<MainPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/notices" element={<NoticesPage />} />
-            <Route path="/friends" element={<OurFriendsPage />} />
-            <Route path="/news" element={<NewsPage />} />
-            <Route path="/user" element={<UserPage />} />
-            <Route path="/add-pet" element={<AddPetPage />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </Suspense>
-    </Provider>
-  );
+  // return (
+  //   <Provider store={store}>
+  //     <Suspense fallback={<p>...Loading</p>}>
+  //       <Routes>
+  //         <Route path="/" element={<SharedLayout />}>
+  //           <Route path="/main" element={<MainPage />} />
+  //           <Route path="/register" element={<RegisterPage />} />
+  //           <Route path="/login" element={<LoginPage />} />
+  //           <Route path="/notices" element={<NoticesPage />} />
+  //           <Route path="/friends" element={<OurFriendsPage />} />
+  //           <Route path="/news" element={<NewsPage />} />
+  //           <Route path="/user" element={<UserPage />} />
+  //           <Route path="/add-pet" element={<AddPetPage />} />
+  //           <Route path="*" element={<NotFound />} />
+  //         </Route>
+  //       </Routes>
+  //     </Suspense>
+  //   </Provider>
+  // );
+
+  return <NoticesFilters>Filter</NoticesFilters>;
 };
---
+++ src/components/Notices/NoticesFilters/handleSendRequest.js
@@ -0,0 +1,13 @@
+// const applyFilters = filters => {
+//   // логічні операції для створення запиту на бекенд з параметрами фільтра
+//   axios
+//     .post('/api/filter', filters)
+//     .then(response => {
+//       // Обробка успішної відповіді від бекенду
+//       console.log(response.data);
+//     })
+//     .catch(error => {
+//       // Обробка помилки
+//       console.error(error);
+//     });
+// };
