184d7637b78e66a61554bddc2193f45bd6680d43
Svitlana <svchernn@gmail.com>
2023-05-17 15:11:20 +0300

    add some change

--- src/components/Notices/NoticesCategoriesNav/NoticesCategoriesNav.jsx
+++ src/components/Notices/NoticesCategoriesNav/NoticesCategoriesNav.jsx
@@ -1,17 +1,68 @@
+import { NavLink} from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { isUserLogin } from 'redux/auth/authSelectors';
 
-import Button from 'shared/components/Button/Button';
 import style from './NoticesCategoriesNav.module.scss';
+import { nanoid } from 'nanoid';
+
+const navBtn = [
+  {
+    id: nanoid(),
+    text: 'sell',
+    link: '/sell',
+  },
+  {
+    id: nanoid(),
+    text: 'lost/found',
+    link: '/lost-found',
+  },
+  {
+    id: nanoid(),
+    text: 'in good hands',
+    link: '/fo-free',
+  },
+
+];
+
+const navAuthBtn = [{
+  id: nanoid(),
+  text: 'favorite ads',
+  link: '/favorite',
+},
+{
+  id: nanoid(),
+  text: 'my ads',
+  link: '/own',
+},
+]
 const NoticesCategoriesNav = () => {
+const isLogin = useSelector(isUserLogin);
+const btn = navBtn.map(({id, text, link}) => <li key={id}>
+<NavLink to={'/notices' + link} className={style.btn}>
+  {text}
+</NavLink></li>);
+
+const authBtn = navAuthBtn.map(({id, text, link}) => <li key={id}>
+  <NavLink to={'/notices'+ link} className ={style.btn}>
+    {text}
+  </NavLink>
+</li>);
+
   return (
-    <div className={style.wrapper}>
-      <Button label={'sell'} className={style.btn} />
+    <ul className={style.wrapper}>
+    {btn} 
+    {isLogin && {authBtn}}
+     
+      {/* <NavLink to="favorite" className={style.btn}>
+      favorite ads
+      </NavLink>
+      <NavLink to="own" className={style.btn}>
+      my ads
+      </NavLink> */}
+    
+    </ul>
 
-      <Button label={'lost/found'} className={style.btn} />
-
-      <Button label={'in good hands'} className={style.btn} />
-    </div>
   );
 };
 
 export default NoticesCategoriesNav;
-
--- src/pages/NoticesPage/NoticesPage.jsx
+++ src/pages/NoticesPage/NoticesPage.jsx
@@ -1,26 +1,37 @@
+import { useNavigate } from 'react-router-dom';
+// import { useParams } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import NoticesSearch from 'components/Notices/NoticesSearch/NoticesSearch';
 import NoticesCategoriesNav from 'components/Notices/NoticesCategoriesNav/NoticesCategoriesNav';
 import PlusIcon from 'icons/PlusSmallIcon';
 import Button from 'shared/components/Button';
-import Title from 'shared/components/Title/Title';
+//
 import style from './NoticesPage.module.scss';
+
 const NoticesPage = () => {
+  const navigate = useNavigate();
+
+  const userAddPet = () => {
+    navigate('/add-pet');
+  };
+
   return (
     <>
       If components are ready, add them to NoticesPage. /NoticesSearch,
       NoticesCategoriesList, NoticesNav, NoticesFilters, AddPet/
-      <Title>Find your favorite pet</Title>
       <NoticesSearch />
       <div className={style.wrapper}>
         <NoticesCategoriesNav />
 
         <Button
+          onClick={userAddPet}
           type="button"
           className={style.addBtn}
           SVGComponent={PlusIcon}
           label={'Add Pet'}
         />
-      </div>{' '}
+      </div>
+      <Outlet />
     </>
   );
 };
