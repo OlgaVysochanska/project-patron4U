bb35d791d3594321c238877492a6b196d706c75f
DianaAkhmedova <106441711+DianaAkhmedova@users.noreply.github.com>
2023-05-18 21:06:52 +0300

    Update NoticesCategoriesList.jsx

--- src/components/Notices/NoticesCategoriesList/NoticesCategoriesList.jsx
+++ src/components/Notices/NoticesCategoriesList/NoticesCategoriesList.jsx
@@ -1,29 +1,17 @@
-import useToggleModalWindow from 'shared/hooks/useToggleModalWindow';
-
-import Modal from 'shared/components/Modal/Modal';
-import NoticeModal from 'components/NoticeModal/NoticeModal';
 import NoticeCategoryItem from '../NoticeCategoryItem';
 
 import styles from './NoticesCategoriesList.module.scss';
-import notice from '../../NoticeModal/notice.json';
 
 const { noticesListContainer, noticesList } = styles;
 
-const NoticesCategoriesList = ({ data }) => {
-  const { isModalOpen, openModal, closeModal } = useToggleModalWindow();
-
-  const noticesItem = data.map(({ id, ...props }) => (
-    <NoticeCategoryItem key={id} openModal={openModal} {...props} />
+const NoticesCategoriesList = ({ notices, loadMore }) => {
+  const noticesItem = notices.map(notice => (
+    <NoticeCategoryItem key={notice._id} loadMore={loadMore} notice={notice} />
   ));
 
   return (
     <div className={noticesListContainer}>
       <ul className={noticesList}>{noticesItem}</ul>
-      {isModalOpen && (
-        <Modal closeModal={closeModal}>
-          <NoticeModal notice={notice} />
-        </Modal>
-      )}
     </div>
   );
 };
