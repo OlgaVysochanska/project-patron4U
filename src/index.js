96cf59393fa46aac24137b305bd0948e22767a93
OlgaVysochanska <olavysochanska@gmail.com>
2023-05-19 11:42:42 +0300

    reexport for authlayout

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
-import AuthLayout from 'components/AuthLayout/AuthLayout';
+import AuthLayout from 'components/AuthLayout';
 import { store, persistor } from 'redux/store';
 import './index.css';
 
