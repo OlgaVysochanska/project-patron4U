5aa5fe7e53444b33e45958869351e36c3b921a48
Rostyk Podolets <ros.podolets@gmail.com>
2023-05-25 11:37:38 +0300

    Scale of user avatar in navigation fixed

--- src/components/UserNav/UserAvatarIcon/UserAvatarIcon.jsx
+++ src/components/UserNav/UserAvatarIcon/UserAvatarIcon.jsx
@@ -1,6 +1,7 @@
 import styles from './UserAvatarIcon.module.scss';
 
 export default function UserAvatarIcon({ src }) {
+  console.log(typeof src);
   return (
     <div className={styles.wrapper}>
       <img className={styles.image} src={src} alt="User Avatar" />
