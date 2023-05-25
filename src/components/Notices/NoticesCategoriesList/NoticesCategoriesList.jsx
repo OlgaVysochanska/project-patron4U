1fd7f3c3c42cab8ad53331434d6a19a8cb2253eb
DianaAkhmedova <106441711+DianaAkhmedova@users.noreply.github.com>
2023-05-26 00:06:56 +0300

    Update NoticesCategoriesList.jsx

--- src/components/Notices/NoticesCategoriesList/NoticesCategoriesList.jsx
+++ src/components/Notices/NoticesCategoriesList/NoticesCategoriesList.jsx
@@ -1,6 +1,7 @@
-// import { useState } from 'react';
 import { useSelector } from 'react-redux';
+
 import useTheme from 'shared/hooks/useTheme';
+
 import NoticeCategoryItem from '../NoticeCategoryItem';
 
 import { isUserLogin } from 'redux/auth/authSelectors';
@@ -8,14 +9,8 @@ import { isUserLogin } from 'redux/auth/authSelectors';
 import styles from './NoticesCategoriesList.module.scss';
 
 const NoticesCategoriesList = ({ notices, loadMore }) => {
-  // const [favoriteArr, setFavoriteArr] = useState([]);
-
   const currentUser = useSelector(isUserLogin);
-  // const favoriteNotices = useSelector(getFavoriteNotices);
 
-  // if (currentUser) {
-  //   setFavoriteArr(favoriteNotices);
-  // }
   const { theme } = useTheme();
 
   const noticesList =
@@ -24,14 +19,12 @@ const NoticesCategoriesList = ({ notices, loadMore }) => {
       : `${styles.noticesList} + ${styles.noticesListDark}`;
 
   const noticesItem = notices.map(notice => {
-    // const myFavoriteNotice = favoriteArr.includes(notice._id);
     return (
       <NoticeCategoryItem
         key={notice._id}
         loadMore={loadMore}
         notice={notice}
         currentUser={currentUser}
-        // myFavoriteNotice={myFavoriteNotice}
       />
     );
   });
