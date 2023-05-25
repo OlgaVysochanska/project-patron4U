79a7479d7e60885f3afdd01d14341293f7c37dc7
28Rostyk <107403659+28Rostyk@users.noreply.github.com>
2023-05-26 01:24:26 +0300

    add theme to userpets

--- src/components/User/PetsData/PetsData.jsx
+++ src/components/User/PetsData/PetsData.jsx
@@ -1,13 +1,20 @@
 import PetsList from './PetsList/PetsList';
 import AddPetButton from 'components/AddPetButton/AddPetButton';
-
+import useTheme from 'shared/hooks/useTheme';
 import styles from './PetsData.module.scss';
 
 const PetsData = () => {
+  const { theme } = useTheme();
   return (
     <div className={styles.petsDataContainer}>
       <div className={styles.petsHeader}>
-        <h2 className={styles.petsTitle}>My pets:</h2>
+        <h2
+          className={`${styles.petsTitle} ${
+            theme === 'dark' && styles.petsTitleDark
+          }`}
+        >
+          My pets:
+        </h2>
         <AddPetButton />
       </div>
       <PetsList />
